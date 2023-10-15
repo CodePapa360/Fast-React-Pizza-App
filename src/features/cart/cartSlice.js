7439359caeba613a970e4567db89cfbfb11eb8cf
codepapa <codepapa360@gmail.com>
2023-10-15 15:55:09 +0600

    deleting-cart-item-functionality

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -64,4 +64,7 @@ export const getTotalCartQuantity = (state) =>
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
 // You can use "reselect" libreary to improve the performence
